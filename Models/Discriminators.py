6175b5291407fad926d82232a02002def43f1cf0
hamza paracha <hamzaparacha098@gmail.com>
2024-03-04 17:39:15 +0500

    removed dummy autoencoder.Further modifications to be done to discriminator

--- Models/Discriminators.py
+++ Models/Discriminators.py
@@ -1,13 +1,14 @@
-from Models.autoencoder_dis import Transformer
+from Utils.Encoder import Encoder
 import torch.nn as nn
+import torch
 from transformers import BertTokenizer, BertModel
+
 
 class Discriminator(nn.Module):
     def __init__(
             self,
             bert_model_name,
-            src_vocab_size,
-            trg_vocab_size,            
+            src_vocab_size,          
             #src_pad_index,
             #trg_pad_index,
             embed_size=64,
@@ -22,15 +23,20 @@ class Discriminator(nn.Module):
         super(Discriminator,self).__init__()
         self.tokenizer=BertTokenizer.from_pretrained(bert_model_name)
         self.model=BertModel.from_pretrained(bert_model_name)
-        self.Transformer=Transformer(src_vocab_size,trg_vocab_size,embed_size,num_layers,forward_expansion,num_heads,dropout,device,max_length)
+        self.Encoder=Encoder(src_vocab_size,embed_size,num_layers,num_heads,device,forward_expansion,dropout,max_length)
         self.device=device
     
 
-    def forward(self,input_sentence,output_sentence):
-        encoded_dict = self.tokenizer(input_sentence,return_tensors='pt')
-        #encoder_embedding=self.model(**encoded_dict).last_hidden_state#[:,-1,:]   
-        decoded_dict=self.tokenizer(output_sentence,return_tensors='pt')     
+    def forward(self,question_sentence,fake_real_sentence):
+        question_dict = self.tokenizer(question_sentence,return_tensors='pt')
+        #encoder_embedding=self.model(**encoded_dict).last_hidden_state#[:,-1,:] 
+        fake_real_dict=self.tokenizer(fake_real_sentence,return_tensors='pt')     
         #decoder_embedding=self.model(**decoded_dict).last_hidden_state#[:,-1,:]
-        concat = encoded_dict + decoded_dict
-        output=self.Transformer(concat)        
+        #concat = question_dict + fake_real_dict
+        # Concatenate tokenized embeddings
+        concat_input = {key: torch.cat([question_dict[key], fake_real_dict[key]], dim=1) for key in question_dict}
+
+        # Convert dictionary of tensors to a single tensor
+        concatenated_tensor = torch.cat(list(concat_input.values()), dim=2)
+        output=self.Encoder(concatenated_tensor,None)        
         return output
