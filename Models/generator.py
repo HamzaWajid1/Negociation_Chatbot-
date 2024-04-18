3a993834648572edfd70b9ce81fdb4d36ac4e45a
hamza paracha <hamzaparacha098@gmail.com>
2024-04-18 13:58:27 +0500

    BertModel Hugging Face used

--- Models/generator.py
+++ Models/generator.py
@@ -17,13 +17,29 @@ class Generator(nn.Module):
             num_heads=8,
             dropout=0,
             device="cuda",
-            max_length=100            
+            max_length=100,
+            vocab_size: int = 30522,
+            hidden_size: int = 768, 
+            num_hidden_layers: int = 12, 
+            num_attention_heads: int = 12, 
+            intermediate_size: int = 3072, 
+            hidden_act: str = "gelu", 
+            hidden_dropout_prob: float = 0.1, 
+            attention_probs_dropout_prob: float = 0.1, 
+            max_position_embeddings: int = 512, 
+            type_vocab_size: int = 2, 
+            initializer_range: float = 0.02, 
+            layer_norm_eps: float = 1e-12, 
+            pad_token_id: int = 0, 
+            position_embedding_type: str = "absolute", 
+            use_cache: bool = True            
 
     ):
         super(Generator,self).__init__()
         self.tokenizer=BertTokenizer.from_pretrained(bert_model_name)
         self.model=BertModel.from_pretrained(bert_model_name)
-        self.Transformer=Transformer(src_vocab_size,trg_vocab_size,embed_size,num_layers,forward_expansion,num_heads,dropout,device,max_length)
+        #self.Transformer=Transformer(src_vocab_size,trg_vocab_size,embed_size,num_layers,forward_expansion,num_heads,dropout,device,max_length)
+        self.Transformer=BertModel(BertConfig(vocab_size=vocab_size,num_hidden_layers=num_hidden_layers,hidden_size=hidden_size,num_attention_heads=num_attention_heads,intermediate_size=intermediate_size,hidden_act=hidden_act,hidden_dropout_prob=hidden_dropout_prob,attention_probs_dropout_prob=attention_probs_dropout_prob,max_position_embeddings=max_position_embeddings,type_vocab_size=type_vocab_size,initializer_range=initializer_range,layer_norm_eps=layer_norm_eps,pad_token_id=pad_token_id,position_embedding_type=position_embedding_type,use_cache=use_cache))
         self.device=device
     
 
